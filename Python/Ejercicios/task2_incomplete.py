22cefcf4c83808770de8e62d9b14a4dcbf1298c5
Araceli <https://etherewind@github.com>
2017-09-26 18:58:24 +0200

    Fichero comentando y añado que ejecute el código generador del sistema de ecuaciones desde el propio programa

--- Python/Ejercicios/task2_incomplete.py
+++ Python/Ejercicios/task2_incomplete.py
@@ -1,10 +1,13 @@
 
 import numpy
 import MyEquation
+import subprocess
 
+#Creamos el fichero system.txt llamando al siguiente archivo
+subprocess.call(["python", "equation-system-generator.py"])
+#Leemos el archivo que nos devolverá la matriz A y el vector b
 A,b = MyEquation.load_equation_system( 'system.txt' )
 print(A,b)
-# Here you have to solve A * x = b, look at in the linear algebra package of numpy for the solution
-
+#numpy.linalg.solve --> Nos devuelve la solución de una equación lineal de matrices de la forma Ax=b
 x = numpy.linalg.solve(A, b)
-print( x )
+print( "Solution: ", x )
