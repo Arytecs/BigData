f0d8aa38440d6220950d94d1816211167b481a17
Araceli <https://etherewind@github.com>
2017-10-03 22:48:21 +0200

    Algunas modificaciones y task2 avanzada

--- Python/Ejercicios/task2_incomplete.py
+++ Python/Ejercicios/task2_incomplete.py
@@ -8,6 +8,7 @@ subprocess.call(["python", "equation-system-generator.py"])
 #Leemos el archivo que nos devolverá la matriz A y el vector b
 A,b = MyEquation.load_equation_system( 'system.txt' )
 print(A,b)
-#numpy.linalg.solve --> Nos devuelve la solución de una equación lineal de matrices de la forma Ax=b
+#numpy.linalg.solve --> Nos devuelve la solución de una equación 
+lineal de matrices de la forma Ax=b
 x = numpy.linalg.solve(A, b)
 print( "Solution: ", x )
