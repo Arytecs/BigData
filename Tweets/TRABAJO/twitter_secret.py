75ab71a8020521910e3d3f06e558576d9ee4d8dc
etherewind <araceliteruel@gmail.com>
2018-07-07 16:43:12 +0200

    Trabajo Tweets

---
+++ Tweets/TRABAJO/twitter_secret.py
@@ -0,0 +1,4 @@
+consumer_key = "Tr9FS85ZnvY0QosL8GuIUoT7M"
+consumer_secret = "uNieqUa7bkNUHuDVdKGZR2pip131YiaSvBriKU2914BJQEBA7m"
+access_token = "129521170-IX1l8pXYNwo7NDAas4J7AymXUI1pMqCrFBvQ4jUV"
+access_token_secret = "1ANlF1ju8mChQjlfFkfY18CrkneFuK2nIVAWGdVKvF6Xn"
